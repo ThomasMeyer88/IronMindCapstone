d118746f3b14f53dcf9eed064b39bf51f4dab23e
Thomas <trexmeyer@gmail.com>
2018-06-29 12:47:09 -0500

    Initial commit

---
+++ src/main/java/com/ironmind/ferrus/FerrusApplication.java
@@ -0,0 +1,12 @@
+package com.ironmind.ferrus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerrusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerrusApplication.class, args);
+	}
+}
