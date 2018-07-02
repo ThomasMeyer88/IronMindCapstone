420855414788866a611f50077c1054c423037a4b
John Wallace <jnwywe@gmail.com>
2018-07-02 16:51:12 -0500

    re-did navbar

--- src/main/java/com/ironmind/ferrus/repositiories/Clients.java
+++ src/main/java/com/ironmind/ferrus/repositiories/Clients.java
@@ -1,7 +1,7 @@
-package com.ironmind.ferrus.repositiories;
-
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface Clients extends CrudRepository<Clients, Long> {
-}
+//package com.ironmind.ferrus.repositiories;
+//
+//
+//import org.springframework.data.repository.CrudRepository;
+//
+//public interface Clients extends CrudRepository<Clients, Long> {
+//}
