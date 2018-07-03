3d5137af267a1a3e4daa911239282cd551810263
John Wallace <jnwywe@gmail.com>
2018-07-03 13:05:57 -0500

    added .cods and application.properties file

---
+++ src/main/java/com/ironmind/ferrus/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.ironmind.ferrus.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "clients/landing";
+    }
+}
+
+
+
--- src/main/java/com/ironmind/ferrus/model/ClientWithRoles.java
+++ src/main/java/com/ironmind/ferrus/model/ClientWithRoles.java
@@ -1,42 +1,42 @@
-//package com.ironmind.ferrus.model;
-//
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.AuthorityUtils;
-//import org.springframework.security.core.userdetails.UserDetails;
-//
-//import java.util.Collection;
-//
-//public class ClientWithRoles extends Client implements UserDetails {
-//
-//        public ClientWithRoles(Client client) {
-//            super(client);
-//        }
-//
-//        @Override
-//        public Collection<? extends GrantedAuthority> getAuthorities() {
-//            String roles = "";
-//            return AuthorityUtils.commaSeparatedStringToAuthorityList(roles);
-//        }
-//
-//        @Override
-//        public boolean isAccountNonExpired() {
-//            return true;
-//        }
-//
-//        @Override
-//        public boolean isAccountNonLocked() {
-//            return true;
-//        }
-//
-//        @Override
-//        public boolean isCredentialsNonExpired() {
-//            return true;
-//        }
-//
-//        @Override
-//        public boolean isEnabled() {
-//            return true;
-//        }
-//    }
-//
+package com.ironmind.ferrus.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class ClientWithRoles extends Client implements UserDetails {
+
+        public ClientWithRoles(Client client) {
+            super(client);
+        }
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            String roles = "";
+            return AuthorityUtils.commaSeparatedStringToAuthorityList(roles);
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return true;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return true;
+        }
+    }
+
 
