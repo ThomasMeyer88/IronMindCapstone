ae868a42eca0f9b046377877357f4d736738d8b0
Thomas <trexmeyer@gmail.com>
2018-07-03 20:11:11 -0500

    Exercise planning page is now partially complete
    Insertions and extractions are operating under settings for a single user and program
    
    CSS is greatly improved
    
    Finished: Dropdown
    Finished: Dragover
    Finished: Exercise Sets are correctly stored by Exercise type, day, program, and user
    
    ToDo: Add search bar to exercise library
    ToDo: Add edit, delete, duplicate functionality to sets as well as sticky form

--- src/main/java/com/ironmind/ferrus/controllers/ExerciseController.java
+++ src/main/java/com/ironmind/ferrus/controllers/ExerciseController.java
@@ -6,11 +6,10 @@ import com.ironmind.ferrus.Services.ExerciseService;
 import com.ironmind.ferrus.Services.subSetService;
 import com.ironmind.ferrus.Services.templateService;
 import com.ironmind.ferrus.Services.workSetService;
-import com.ironmind.ferrus.model.Exercise;
-import com.ironmind.ferrus.model.SubSet;
-import com.ironmind.ferrus.model.WorkSet;
-import com.ironmind.ferrus.model.template;
+import com.ironmind.ferrus.model.*;
+import com.ironmind.ferrus.repositiories.ClientRepositories;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -24,15 +23,20 @@ public class ExerciseController {
     private workSetService workDao;
     private templateService tempDao;
     private subSetService setDao;
+    private programService programDao;
+    private ClientRepositories clientDao;
 
     @Autowired
     public ExerciseController
             (ExerciseService exerciseService, workSetService work, templateService tempDao,
-             subSetService setDao){
+             subSetService setDao, programService programService, ClientRepositories clientDao){
         this.exerciseService = exerciseService;
         this.workDao = work;
         this.tempDao = tempDao;
         this.setDao = setDao;
+        this.programDao = programService;
+        this.clientDao = clientDao;
+
     }
 
     @GetMapping("/tests")
@@ -48,15 +52,17 @@ public class ExerciseController {
         return("posts/index");
     }
 
-    @GetMapping("/exercises/{day}")
-    public String exercisesIndex(@PathVariable long day, Model view){
-        List<WorkSet> daySet = workDao.getWork().findAllByTemplate(tempDao.getTemplates().findOne(1L));
+    @GetMapping("/exercises/{name}/{day}")
+    public String exercisesIndex(@PathVariable int day, @PathVariable String name, Model view){
+        template temp = tempDao.getTemplates().findByProgram_IdAndDay(3, day);
+        List<WorkSet> daySet = workDao.getWork().findAllByTemplate(temp);
         System.out.println(daySet.size());
         SubSet subSet = new SubSet(0, 0, " ", null);
         view.addAttribute("workSets", daySet);
         view.addAttribute("subSet", subSet);
         view.addAttribute("exercises", exerciseService.getExercises().findAll());
         view.addAttribute("day", day);
+        view.addAttribute("name", name);
         return("exercises/exercises");
     }
 
@@ -75,18 +81,48 @@ public class ExerciseController {
         return "redirect:/exercises";
     }
 
-    @PostMapping("/createplan/{day}")
-    public String createDay(@PathVariable long day, SubSet subSet){
+    @PostMapping("/createplan/{name}/{day}")
+    public String createDay(@PathVariable long day, @PathVariable String name, SubSet subSet){
         System.out.println(subSet.getExerciseName());
-        WorkSet newWorkSet = new WorkSet(subSet.getExerciseName(), exerciseService.getExercises().findByName(subSet.getExerciseName()));
-        workDao.getWork().save(newWorkSet);
-        SubSet newSubSet = subSet;
-        template temp = new template((int)day);
-        tempDao.getTemplates().save(temp);
-        newWorkSet.setTemplate(temp);
-        newSubSet.setWorkSet(newWorkSet);
-        setDao.getSets().save(newSubSet);
-        return "redirect:/exercises/" + day;
+        Client clientSession = (Client) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        try{
+            try {
+                Program program = programDao.getPrograms().findByClient_IdAndName(clientSession.getId(), name);
+                System.out.println("First try program " + program.getId());
+                template temp = tempDao.getTemplates().findByProgram_IdAndDay(program.getId(), (int)day);
+                System.out.println("First try temp " + temp.getId());
+                WorkSet work = workDao.getWork().findByTemplate_IdAndExerciseName(temp.getId(), subSet.getExerciseName());
+                work.setTemplate(temp);
+                System.out.println("Work ID AND TEMPLATE");
+                System.out.println(work.getId() + " " + work.getTemplate());
+                subSet.setWorkSet(work);
+                setDao.getSets().save(subSet);
+                System.out.println(subSet.getWorkSet().getTemplate());
+                return "redirect:/exercises/" + name + "/" + day;
+            } catch (NullPointerException e) {
+                Program program = programDao.getPrograms().findByClient_IdAndName(clientSession.getId(), name);
+                System.out.println("First try program " + program.getId());
+                template temp = tempDao.getTemplates().findByProgram_IdAndDay(program.getId(), (int)day);
+                System.out.println("First try temp " + temp.getId());
+                WorkSet work = new WorkSet(subSet.getExerciseName(), exerciseService.getExercises().findByName(subSet.getExerciseName()));
+                work.setTemplate(temp);
+                workDao.getWork().save(work);
+                subSet.setWorkSet(work);
+                setDao.getSets().save(subSet);
+                return "redirect:/exercises/" + name + "/" + day;
+            }
+
+        } catch (NullPointerException e){
+            template temp = new template((int)day);
+            temp.setProgram(programDao.getPrograms().findByClient_IdAndName(clientSession.getId(), name));
+            tempDao.getTemplates().save(temp);
+            WorkSet work = new WorkSet(subSet.getExerciseName(),exerciseService.getExercises().findByName(subSet.getExerciseName()));
+            work.setTemplate(temp);
+            workDao.getWork().save(work);
+            subSet.setWorkSet(work);
+            setDao.getSets().save(subSet);
+            return "redirect:/exercises/" + name + "/" + day;
+        }
     }
 
     @RequestMapping(value = "/editplan/{day}", method = RequestMethod.POST)
---
+++ src/main/java/com/ironmind/ferrus/model/programService.java
@@ -0,0 +1,27 @@
+package com.ironmind.ferrus.model;
+
+import com.ironmind.ferrus.repositiories.Programs;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.ironmind.ferrus.repositiories.templates;
+
+@Service
+public class programService {
+    private Programs programs;
+
+
+    public programService(){};
+
+    @Autowired
+    public programService(Programs programs) {
+        this.programs = programs;
+    }
+
+    public Programs getPrograms() {
+        return programs;
+    }
+
+    public void setPrograms(Programs programs) {
+        this.programs = programs;
+    }
+}
