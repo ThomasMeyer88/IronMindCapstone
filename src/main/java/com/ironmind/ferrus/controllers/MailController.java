53588f1a97beebc3c57c179deb7af2f21a4d4a80
John Wallace <jnwywe@gmail.com>
2018-07-16 13:40:10 -0500

    stuffs

--- src/main/java/com/ironmind/ferrus/controllers/MailController.java
+++ src/main/java/com/ironmind/ferrus/controllers/MailController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import static javax.mail.Message.RecipientType.BCC;
+//import static javax.mail.Message.RecipientType.BCC;
 import static org.simplejavamail.mailer.config.TransportStrategy.SMTP_TLS;
 
 
